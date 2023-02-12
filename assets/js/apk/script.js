37c6069dc55a520484ec64360b839d0260288932
Myprofilee <andrep13.id@gmail.com>
2023-02-12 09:34:23 +0700

    first commit

--- assets/js/apk/script.js
+++ assets/js/apk/script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Feb 13, 2023 00:00:00").getTime();
+var countDownDate = new Date("Feb 14, 2023 00:00:00").getTime();
 
 var x = setInterval(function () {
 
--- assets/js/total.js
+++ assets/js/total.js
@@ -1,3 +1,3 @@
 /* Nella Kharisma */
-document.getElementById("total-lagu-nella-kharisma").textContent = "2";
+document.getElementById("total-lagu-nella-kharisma").textContent = "3";
 document.getElementById("tahun-lagu-nella-kharisma").textContent = "2011";
